15aa709a88fa5aa32102ff14dabcd292490effee
dev-rahman96 <68157326+dev-rahman96@users.noreply.github.com>
2021-02-18 15:45:56 -0500

    Launched EC2 with docker

--- class.tf
+++ class.tf
@@ -49,3 +49,20 @@ resource "aws_s3_bucket" "bucket-1" {
   }
 }
 
+# 4. Create an EC2 and install docker
+resource "aws_instance" "ec2_instance" {
+  ami           = "ami-047a51fa27710816e" # us-east-1
+  instance_type = "t2.micro"
+  availability_zone = "us-east-1a"
+  user_data = <<-EOF
+		#!/bin/bash
+    sudo yum update -y
+    sudo amazon-linux-extras install docker -y
+    sudo yum install docker git python3 -y
+    sudo service docker start
+    sudo usermod -a -G docker ec2-user
+    EOF
+    tags = {
+      Name = "EC2 With Docker"
+    }
+}
